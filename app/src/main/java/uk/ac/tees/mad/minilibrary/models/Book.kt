43f312a8178acd83ed4ca429508a26746d0c905f
Akshay Patchala <minilibrary7213@gmail.com>
2026-03-23 12:00:00 +0530

    Updated books model calsswith serialisation

--- app/src/main/java/uk/ac/tees/mad/minilibrary/models/Book.kt
+++ app/src/main/java/uk/ac/tees/mad/minilibrary/models/Book.kt
@@ -1,11 +1,22 @@
 package uk.ac.tees.mad.minilibrary.models
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Book(
+    @SerialName("id")
     val id: String = "",
+    @SerialName("user_id")
     val userId: String = "",
+    @SerialName("title")
     val title: String = "",
+    @SerialName("subject")
     val subject: String = "",
+    @SerialName("file_name")
     val fileName: String = "",
+    @SerialName("file_url")
     val fileUrl: String = "",
+    @SerialName("uploaded_at")
     val uploadedAt: Long = System.currentTimeMillis()
 )
